37db971cb7950cc62ab028e5511a721855c3bba3
grizeus <zaytsewww@gmail.com>
2023-07-13 00:16:35 +0300

    check for renderable main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "../include/SDL_Tools.h"
+#include "../include/Player.h"
+
 
 int main(int argc, char** argv) {
-	SDL_Tools* tool = new SDL_Tools();
+	// SDL_Tools* tool = new SDL_Tools();
+	ToolsPtr tool = std::make_shared<SDL_Tools>();
 	tool->ToolsInit();
+	Player player(tool);
 	std::cout << "Hello from crimson game!" << std::endl;
+	while(true) {
+		SDL_RenderClear(tool->m_Renderer);
+		player.Draw();
+		SDL_RenderPresent(tool->m_Renderer);
+	}
 	tool->ToolsQuit();
-	delete tool;
 	return 0;
 }
